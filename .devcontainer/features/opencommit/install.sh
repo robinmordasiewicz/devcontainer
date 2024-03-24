3c0cc733fa817d272bf8ac84aed7576a52a449c6
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-24 10:36:34 -0400

    updating devcontainer

--- .devcontainer/features/opencommit/install.sh
+++ .devcontainer/features/opencommit/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+<<<<<<< HEAD
 set -e
 
 WORKINGDIR=$(pwd)
@@ -17,3 +18,27 @@ oco hook set
 #cd /tmp/opencommit &&  git apply ${REPODIR}/.devcontainer/opencommit-ollama.patch
 #sudo -H env PATH=$PATH npm install -g
 #cd ${REPODIR}
+=======
+#
+
+directory="/tmp/opencommit"
+old_string='"http://localhost:11434/api/chat"'
+new_string='"http://commit.mordasiewicz.com:11434/api/chat"'
+
+if [ -d "${directory}" ]; then
+  rm -rf "${directory}"
+fi
+git clone https://github.com/di-sukharev/opencommit.git "${directory}"
+
+find "${directory}" -type f -print0 | while IFS= read -r -d '' file
+do
+    if grep -q "${old_string}" "${file}"; then
+        sed -i '' "s|${old_string}|${new_string}|g" "${file}"
+    fi
+done
+
+npm uninstall -g opencommit
+cd "${directory}" && npm install -g
+
+#oco hook set
+>>>>>>> 74309fa (update config)
