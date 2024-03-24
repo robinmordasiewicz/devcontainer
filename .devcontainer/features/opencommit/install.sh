86017128275e9b78424adcc0079f2d6acfe5b29e
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-24 13:46:17 -0400

    chore(opencommit-feature): update opencommit feature version to 0.0.3 in devcontainer-feature.json chore(opencommit-install): change hardcoded localhost URL to remote server URL for opencommit API in install script refactor(opencommit-install): improve sed command usage by using a temporary file instead of inplace editing and remove unnecessary directory reference from cd command. perf(opencommit-install): optimize find command usage by using recursive search and removing unnecessary variable assignment. docs(opencontainer-features): update opencommit feature documentation with new version number and remote server URL for API.

--- .devcontainer/features/opencommit/install.sh
+++ .devcontainer/features/opencommit/install.sh
@@ -2,20 +2,23 @@
 #
 
 directory="/tmp/opencommit"
-old_string='"http://localhost:11434/api/chat"'
-new_string='"http://commit.mordasiewicz.com:11434/api/chat"'
+old_string="http://localhost:11434/api/chat"
+new_string="http://commit.mordasiewicz.com:11434/api/chat"
 
 if [ -d "${directory}" ]; then
   rm -rf "${directory}"
 fi
 git clone https://github.com/di-sukharev/opencommit.git "${directory}"
 
-find "${directory}" -type f -print0 | while IFS= read -r -d '' file
+cd ${directory}
+find . -type f -print0 | while IFS= read -r -d '' file
 do
     if grep -q "${old_string}" "${file}"; then
-        sed -i '' "s|${old_string}|${new_string}|g" "${file}"
+        echo $file && sed "s|${old_string}|${new_string}|g" "${file}" > "${file}.tmp" && mv -f "${file}.tmp" "${file}"
     fi
 done
 
 npm uninstall -g opencommit
 cd "${directory}" && npm install -g
+cd -
+rm -rf ${directory}
