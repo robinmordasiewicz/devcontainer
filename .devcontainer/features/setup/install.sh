381d8932fbc72962457d727850a4bb7716a4511e
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-24 10:59:34 -0400

    chore(setup): add "ghcr.io/devcontainers-contrib/features/npm-package" as a dependency in devcontainer-feature.json for automatic npm installation ci(setup): remove global opencommit installation from install.sh script to avoid conflicts and use the one provided by the npm package instead

--- .devcontainer/features/setup/install.sh
+++ .devcontainer/features/setup/install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "Activating feature 'setup'"
-
 echo "$_REMOTE_USER ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/$_REMOTE_USER
 
 npm install -g npm@latest
-npm install -g opencommit
 
 apt update
 apt-get -y upgrade
