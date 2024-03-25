78918e0cef32e062a13c69d5a0691c0a6247388e
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-25 13:37:09 -0400

    update-feature(update-conda): increment devcontainer-feature.json version to 0.0.3 and update install.sh script to use "defaults" channel instead of "conda-forge" for condas update chore(update-conda): modify the install.sh script to update all packages in the base environment using the defaults channel and add a new command to update the conda repository metadata file.

--- .devcontainer/features/update-conda/install.sh
+++ .devcontainer/features/update-conda/install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "Activating feature 'update conda'"
-
-su -l $_REMOTE_USER -c "source /opt/conda/etc/profile.d/conda.sh && yes y | /opt/conda/bin/conda update -n base -c conda-forge conda"
 su -l $_REMOTE_USER -c "source /opt/conda/etc/profile.d/conda.sh && yes y | /opt/conda/bin/conda update --all"
 su -l $_REMOTE_USER -c "source /opt/conda/etc/profile.d/conda.sh && yes y | /opt/conda/bin/conda update -n base -c conda-forge conda --repodata-fn=repodata.json"
+su -l $_REMOTE_USER -c "source /opt/conda/etc/profile.d/conda.sh && yes y | /opt/conda/bin/conda update -n base -c defaults conda --repodata-fn=repodata.json"
 
 su -l $_REMOTE_USER -c "/opt/conda/bin/conda init --all"
 su -l $_REMOTE_USER -c "/opt/conda/bin/conda config --set changeps1 False"
