06bb384ecac4ea6386b26c4202bb11602a133a70
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-25 18:31:02 -0400

    refactor(preinstall-extensions/install.sh): add function get\_latest\_release to fetch the latest commit sha of a GitHub repository and use it for installing Visual Studio Code extensions docs(preinstall-scripts): update preinstall-vscode-extensions.sh script to use new get\_latest\_release function for getting the latest Visual Studio Code extension version.

--- .devcontainer/features/preinstall-extensions/install.sh
+++ .devcontainer/features/preinstall-extensions/install.sh
@@ -1,26 +1,49 @@
 #!/bin/env bash
 set -e
 
-# USAGE:
-# COPY .devcontainer/ /app/.devcontainer/
-# ARG VSCODE_VERSION=2ccd690cbff1569e4a83d7c43d45101f817401dc
-# RUN bash .devcontainer/preinstall-vscode-extensions.sh $VSCODE_VERSION /app/.devcontainer/devcontainer.json
-# 
-#Version: 1.87.2
-#Commit: 863d2581ecda6849923a2118d93a088b0745d9d6
-#Date: 2024-03-08T15:14:59.643Z
-
 EXTENSIONS=${EXTENSIONS:-undefined}
 
-vscode_commit_sha="863d2581ecda6849923a2118d93a088b0745d9d6"
+get_latest_release() {
+    tag=$(curl --silent "https://api.github.com/repos/${1}/releases/latest" | # Get latest release from GitHub API
+          grep '"tag_name":'                                              | # Get tag line
+          sed -E 's/.*"([^"]+)".*/\1/'                                    ) # Pluck JSON value
+
+    tag_data=$(curl --silent "https://api.github.com/repos/${1}/git/ref/tags/${tag}")
+
+    sha=$(echo "${tag_data}"           | # Get latest release from GitHub API
+          grep '"sha":'                | # Get tag line
+          sed -E 's/.*"([^"]+)".*/\1/' ) # Pluck JSON value
+
+    sha_type=$(echo "${tag_data}"           | # Get latest release from GitHub API
+          grep '"type":'                    | # Get tag line
+          sed -E 's/.*"([^"]+)".*/\1/'      ) # Pluck JSON value
+
+    if [ "${sha_type}" != "commit" ]; then
+        combo_sha=$(curl -s "https://api.github.com/repos/${1}/git/tags/${sha}" | # Get latest release from GitHub API
+              grep '"sha":'                                                     | # Get tag line
+              sed -E 's/.*"([^"]+)".*/\1/'                                      ) # Pluck JSON value
+
+        # Remove the tag sha, leaving only the commit sha;
+        # this won't work if there are ever more than 2 sha,
+        # and use xargs to remove whitespace/newline.
+        sha=$(echo "${combo_sha}" | sed -E "s/${sha}//" | xargs)
+    fi
+
+    printf "${sha}"
+}
 
 ARCH="x64"
 U_NAME=$(uname -m)
+
 if [ "${U_NAME}" = "aarch64" ]; then
     ARCH="arm64"
 fi
 
 archive="vscode-server-linux-${ARCH}.tar.gz"
+owner='microsoft'
+repo='vscode'
+vscode_commit_sha=$(get_latest_release "${owner}/${repo}")
+
 vscode_dir="/tmp/.vscode/"
 if [ ! -d  ${vscode_dir} ]; then
     su -l $_REMOTE_USER -c "mkdir -p ${vscode_dir}"
