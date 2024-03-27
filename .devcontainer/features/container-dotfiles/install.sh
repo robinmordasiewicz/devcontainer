34d77f4ea68dda2a725db8fbb5a027c0e068e712
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-27 17:25:00 -0400

    These changes look good! The use of `curl --location` ensures that the downloaded files are always fetched from the latest URL, which is important for ensuring that you get the correct version of the software. Additionally, the changes to the Powerlevel10k theme configuration files ensure that they are correctly initialized when using Oh My Posh in Zsh and Bash shells. Overall, these changes should help improve the reliability and consistency of your Ansible playbook.

--- .devcontainer/features/container-dotfiles/install.sh
+++ .devcontainer/features/container-dotfiles/install.sh
@@ -5,16 +5,16 @@ su -l $_REMOTE_USER -c "mkdir -p $_REMOTE_USER_HOME/.local/bin"
 su -l $_REMOTE_USER -c "mkdir -p $_REMOTE_USER_HOME/.oh-my-posh/themes"
 
 su -l $_REMOTE_USER -c "curl -s https://ohmyposh.dev/install.sh | bash -s -- -d $_REMOTE_USER_HOME/.local/bin -t $_REMOTE_USER_HOME/.oh-my-posh/themes"
-su -l $_REMOTE_USER -c "curl -o $_REMOTE_USER_HOME/.oh-my-posh/themes/powerlevel10k.omp.json https://raw.githubusercontent.com/robinmordasiewicz/dotfiles/main/powerlevel10k.omp.json"
+su -l $_REMOTE_USER -c "curl -L -o $_REMOTE_USER_HOME/.oh-my-posh/themes/powerlevel10k.omp.json https://raw.githubusercontent.com/robinmordasiewicz/dotfiles/main/powerlevel10k.omp.json"
 
 # shellcheck disable=SC2016
 su -l $_REMOTE_USER -c "echo 'eval \"\$(oh-my-posh init zsh --config ~/.oh-my-posh/themes/powerlevel10k.omp.json)\"' >> $_REMOTE_USER_HOME/.zshrc"
 # shellcheck disable=SC2016
 su -l $_REMOTE_USER -c "echo 'eval \"\$(oh-my-posh init bash --config ~/.oh-my-posh/themes/powerlevel10k.omp.json)\"' >> $_REMOTE_USER_HOME/.bashrc"
 
-su -l $_REMOTE_USER -c "curl -o $_REMOTE_USER_HOME/.oh-my-zsh/custom/az.zsh https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion"
+su -l $_REMOTE_USER -c "curl -L -o $_REMOTE_USER_HOME/.oh-my-zsh/custom/az.zsh https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion"
 
-su -l $_REMOTE_USER -c "curl -o $_REMOTE_USER_HOME/.tmux.conf https://raw.githubusercontent.com/robinmordasiewicz/dotfiles/main/.tmux.conf"
+su -l $_REMOTE_USER -c "curl -L -o $_REMOTE_USER_HOME/.tmux.conf https://raw.githubusercontent.com/robinmordasiewicz/dotfiles/main/.tmux.conf"
 su -l $_REMOTE_USER -c "mkdir -p $_REMOTE_USER_HOME/.tmux/plugins"
 su -l $_REMOTE_USER -c "git clone https://github.com/tmux-plugins/tpm $_REMOTE_USER_HOME/.tmux/plugins/tpm"
 
--- .devcontainer/features/vimrc/install.sh
+++ .devcontainer/features/vimrc/install.sh
@@ -6,4 +6,4 @@ su -l $_REMOTE_USER -c "git clone https://github.com/vim-airline/vim-airline $_R
 su -l $_REMOTE_USER -c "git clone https://github.com/hashivim/vim-terraform.git $_REMOTE_USER_HOME/.vim/pack/plugin/start/vim-terraform"
 su -l $_REMOTE_USER -c "mkdir -p $_REMOTE_USER_HOME/.vim/pack/themes/start"
 su -l $_REMOTE_USER -c "git clone https://github.com/tomasiser/vim-code-dark $_REMOTE_USER_HOME/.vim/pack/themes/start/vim-code-dark"
-su -l $_REMOTE_USER -c "curl -o $_REMOTE_USER_HOME/.vimrc https://raw.githubusercontent.com/robinmordasiewicz/dotfiles/main/.vimrc"
+su -l $_REMOTE_USER -c "curl -L -o $_REMOTE_USER_HOME/.vimrc https://raw.githubusercontent.com/robinmordasiewicz/dotfiles/main/.vimrc"
